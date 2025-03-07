388c0c58d5d334f8d48c753f4f0f80750bba8a2b
lc-dev258 <lucas.sant2802@gmail.com>
2025-03-07 03:45:03 +0000

    supertrunfo

---
+++ supertrunfo.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// definir as variáveis 
+
+int main (){
+  
+  int populacaoA = 23566;
+  int PopulacaoB = 25233;
+  float areaA = 235.7;
+  float AreaB;
+  float pibA;
+  float PibB;
+  int pontosTuristicosA;
+  int PontosTuristicosB;
+  
+  //interação com o usuário
+  
+  
+  printf("População do estado A\n");
+  scanf(" %d", &populacaoA);
+  
+  printf("População do estado B\n");
+  scanf("%d", &PopulacaoB);
+  
+  printf("Area do estado A\n");
+  scanf("%f", &areaA);
+  
+  printf("Area do estado B\n");
+  scanf("%f", &AreaB);
+  
+  printf("PIB do estado A\n");
+  scanf("%f", &pibA);
+  
+  printf("PIB do estado B\n");
+  scanf("%f", &PibB);
+  
+  printf("Pontos turísticos do estado A\n");
+  scanf("%d", &pontosTuristicosA);
+  
+  printf("Pontos turísticos do estado B\n");
+  scanf("%d", &PontosTuristicosB);
+  
+  //comparação entre as cidades
+  
+  printf("----------ESTADO A----------\n");
+  
+  printf("População do estado A: %d\n", populacaoA);
+  printf("Area do estado A: %f\n", areaA);
+  printf("PIB do estado A: %f\n", pibA );
+  printf("Pontos turísticos do estado A: %d\n", pontosTuristicosA);
+ 
+  printf("----------ESTADO B----------\n");
+  
+  printf("População do estado: %d\n", PopulacaoB);
+  printf("Area do estado: %f\n", AreaB);
+  printf("PIB do estado: %f\n", PibB );
+  printf("Pontos turísticos do estado: %d\n", PontosTuristicosB);
+  
+  return 0;
+}
